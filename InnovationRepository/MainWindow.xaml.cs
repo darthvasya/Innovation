322ce809536dab94832ccd593a5d8ae1fc350815
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-05-04 09:25:27 +0300

    Add first design sketch.

--- InnovationRepository/MainWindow.xaml.cs
+++ InnovationRepository/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace InnovationRepository
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("s");
+        }
     }
 }
